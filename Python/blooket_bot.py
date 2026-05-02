6fcb1e5cbe9bd57da27115c1656c027a5b1ba289
BossIion <ulysse.schaff@gmail.com>
2026-05-02 12:31:12 -0400

    Updated to work with Pytohn 3.14 and most recent versions of UC and Selenium + added verification

--- Python/blooket_bot.py
+++ Python/blooket_bot.py
@@ -5,52 +5,72 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.service import Service
 
 
 import pyautogui as pg
 import time
 
-
-beta_chrome_path = "c:\\Program Files\\Google\\Chrome Beta\\Application\\chrome.exe"
-gameIDXPATH = r"/html/body/main/div/form/div[1]/div/div/input"
-gameIDSubmitXPATH = r"/html/body/main/div/form/div[1]/div/div/button"
-nicknameSumbitXPATH = r"/html/body/div/div/div/div[2]/div/form/div[2]/div[2]/button"
-nicknameXPATH = r"/html/body/div/div/div/div[2]/div/form/div[2]/div[1]/input"
-errorXPATH = r"/html/body/section/div/div/div[1]"
-
-
+if __name__ == "__main__":
+    beta_chrome_path = r"C:\Program Files\Google\Chrome Beta\Application\chrome.exe"
+    gameIDXPATH = r"/html/body/main/div/form/div[1]/div/div/input"
+    gameIDSubmitXPATH = r"/html/body/main/div/form/div[1]/div/button"
+    nicknameSumbitXPATH = r"/html/body/div/div/div/div[2]/div/form/div[2]/div[2]"
+    nicknameXPATH = r"/html/body/div/div/div/div[2]/div/form/div[2]/div[1]/input"
+    errorXPATH = r"/html/body/section/div/div/div[1]"
 
 
-def getGameID():
-    gameID = ""
-    while type(gameID) != int:
-        try:
-            user_input = str(pg.prompt(text="Input Blooket Game ID", title="Input Game ID", default = '')).strip()
-            gameID = int(user_input)
-        except:
+
+
+    def getGameID():
+        gameID = ""
+        while type(gameID) != int:
+            try:
+                user_input = str(pg.prompt(text="Input Blooket Game ID", title="Input Game ID", default = '')).strip()
+                gameID = int(user_input)
+            except:
+                pg.alert(text="Invalid Game ID, try again", title="Invalid Game ID", button="OK")
+        return gameID
+
+
+    gameID = getGameID()
+    verifiedGameID = False
+
+    options = uc.ChromeOptions()
+    driver = uc.Chrome(browser_executable_path=beta_chrome_path, options=options)
+
+    wait = WebDriverWait(driver, 10)
+
+    driver.get("https://play.blooket.com/play")
+
+
+    ID_SubmitButton = wait.until(EC.element_to_be_clickable((By.XPATH, gameIDSubmitXPATH)))
+    ID_input_box = wait.until(EC.element_to_be_clickable((By.XPATH, gameIDXPATH)))
+    ID_input_box.clear()
+    for char in str(gameID):
+        ID_input_box.send_keys(char)
+        time.sleep(0.2)
+    ID_SubmitButton.click()
+
+    # Verify Game ID
+
+    try:
+        error = driver.find_element(By.XPATH, errorXPATH)
+        if error.is_displayed():
+            verifiedGameID = False
             pg.alert(text="Invalid Game ID, try again", title="Invalid Game ID", button="OK")
+    except:
+        verifiedGameID = True
 
+    if verifiedGameID:
+        nickname_input_box = wait.until(EC.element_to_be_clickable((By.XPATH, nicknameXPATH)))
+        nickname_submit_button = wait.until(EC.element_to_be_clickable((By.XPATH, nicknameSumbitXPATH)))
 
-gameID = getGameID()
-verifiedGameID = False
+        nickname = "TESTESTESTEST"
 
-#service = Service(executable_path=beta_chrome_path)
+        nickname_input_box.clear()
+        for char in nickname:
+            nickname_input_box.send_keys(char)
+            time.sleep(0.2)
+        nickname_submit_button.click()
 
-#options = uc.ChromeOptions()
-driver = uc.Chrome()
-
-wait = WebDriverWait(driver, 10)
-
-driver.get("https://play.blooket.com/play")
-
-
-ID_SubmitButton = wait.until(EC.element_to_be_clickable((By.XPATH, gameIDSubmitXPATH)))
-ID_input_box = driver.find_element(By.XPATH, gameIDXPATH)
-ID_input_box.clear()
-for char in str(gameID):
-    ID_input_box.send_keys(char)
-    time.sleep(0.2)
-ID_SubmitButton.click()
-
-driver.quit()
+    driver.quit()
